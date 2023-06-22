d829206fae9041eca4b6f8c78ba4438e5a1f6ee1
LideChennn <2030502103@gdpu.edu.cn>
2023-06-22 15:06:15 +0800

    进行了数据测试，但效果不明显

--- test.py
+++ test.py
@@ -1,32 +1,81 @@
-# import torch
-# import torchvision
-# from PIL import Image
-# from model.resnet import net, ResNetModel
-#
-# targets = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-#
-# img_path = './data/imgs/img_8.png'
-# img = Image.open(img_path)
-# img = img.convert('RGB')
-# print(img)
-#
-# transform = torchvision.transforms.Compose([
-#     torchvision.transforms.Resize((32, 32)),
-#     torchvision.transforms.ToTensor()
-# ])
-#
-# img = transform(img)
-# img = img.cuda()
-# print(img.shape)
-#
-# model = torch.load("./weight/resnet39.pth")
-#
-# print(model)
-# img = torch.reshape(img, (1, 3, 32, 32))
-#
-# model.eval()
-# with torch.no_grad():
-#     output = model(img)
-#
-# print(output)
-# print(targets[output.argmax(1)])
+import torch
+import os
+import numpy as np
+from model.resnet import FaceNetModel
+from PIL import Image
+from torchvision import transforms
+
+from util.object_detect import object_detect
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# 加载训练好的模型
+model_path = "./weight/facenet_checkpoint16.pth"
+facenet = FaceNetModel().to(device)
+facenet.load_state_dict(torch.load(model_path))
+facenet.eval()
+
+# 计算阈值，或从训练过程中确定
+threshold = 1.0
+
+
+# 对一张图像预处理
+def preprocess(image_path):
+    # 目标检测，检测到人脸
+    img = object_detect(img_dir=image_path)[0]
+    pil_image = Image.fromarray(img)
+
+    transform = transforms.Compose([
+        transforms.Resize((160, 160)),
+        transforms.ToTensor(),
+    ])
+    return transform(pil_image).unsqueeze(0)
+
+
+# 计算两个向量之间的欧氏距离
+def euclidean_distance(a, b):
+    return np.linalg.norm(a - b)
+
+
+# 从文件夹中加载人脸库
+face_database_path = "./data/face/database"
+face_database = {}
+for person_name in os.listdir(face_database_path):
+    person_folder = os.path.join(face_database_path, person_name)
+    if person_name not in face_database:
+        face_database[person_name] = []
+    for image_name in os.listdir(person_folder):
+        image_path = os.path.join(person_folder, image_name)
+
+        # 处理路径得到图片张量
+        image_tensor = preprocess(image_path).to(device)
+        with torch.no_grad():
+            embedding = facenet(image_tensor).cpu().numpy()
+        face_database[person_name].append(embedding)
+
+# 读取待识别的图像
+test_image_path = "./data/face/test/cxk"
+test_image_tensor = preprocess(test_image_path).to(device)
+
+# 提取待识别图像的特征向量
+with torch.no_grad():
+    test_embedding = facenet(test_image_tensor).cpu().numpy()
+
+# 计算待识别图像与人脸库中所有图像的欧氏距离
+distances = {}
+for person_name, embeddings in face_database.items():
+    min_distance = float('inf')
+    for embedding in embeddings:
+        distance = euclidean_distance(test_embedding, embedding)
+        min_distance = min(min_distance, distance)
+    distances[person_name] = min_distance
+
+# 根据阈值判断待识别图像与人脸库中哪个人脸最接近
+closest_name, closest_distance = min(distances.items(), key=lambda x: x[1])
+if closest_distance <= threshold:
+    print("识别结果: {}，距离: {:.4f}".format(closest_name, closest_distance))
+else:
+    print("未识别到匹配的人脸")
+
+for person_name, distance in distances.items():
+    print(f"与 {person_name} 的最小距离: {distance:.4f}")
--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from sklearn.metrics import precision_recall_fscore_support
 from torch.utils.data import random_split, DataLoader
 from torchvision.transforms import transforms
-
+import numpy as np
 from model.resnet import FaceNetModel
+import torch.nn.functional as F
 from util.dataset import LFWTripletDataset
 
 # 定义训练设备cpu
@@ -13,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 lfw_root = "./data/lfw"
 dataset = LFWTripletDataset(root_dir=lfw_root,
                             transform=transforms.Compose([
-                                transforms.Resize((112, 112)),
+                                transforms.Resize((96, 96)),
                                 transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                             ]))
@@ -23,17 +25,23 @@ train_size = int(0.7 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=8, shuffle=False)
+train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=2, shuffle=False)
 
+# 模型
 facenet = FaceNetModel().to(device)
 
-loss_function = nn.TripletMarginLoss(margin=1.0)
+# 损失函数
+loss_function = nn.TripletMarginLoss(margin=1.0, p=2)
 
-optimizer = optim.Adam(facenet.parameters(), lr=0.05)
+# 优化器
+optimizer = optim.Adam(facenet.parameters(), lr=0.0001)
+# 自动调节学习率
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
 total_train_step = 0  # 记录训练的次数
 total_test_step = 0  # 记录测试的次数
+least_loss = float('inf')
 epochs = 20
 
 for epoch in range(1, epochs + 1):
@@ -60,8 +68,13 @@ for epoch in range(1, epochs + 1):
         if total_train_step % 100 == 0:
             print("训练次数:{}, loss:{}".format(total_train_step, loss))
 
+    # 积累机会，调用道step_size = 10 就会更新学习率
+    scheduler.step()
+
     facenet.eval()
-    test_loss = 0
+    # 初始化距离和标签列表
+    distances = []
+    labels = []
 
     with torch.no_grad():
         for anchors, positives, negatives in test_dataloader:
@@ -71,10 +84,29 @@ for epoch in range(1, epochs + 1):
             positives_embeddings = facenet(positives)
             negatives_embeddings = facenet(negatives)
 
-            loss = loss_function(anchors_embeddings, positives_embeddings, negatives_embeddings)
+            positive_distances = F.pairwise_distance(anchors_embeddings, positives_embeddings, p=2.0)  # 计算欧式举例
+            negative_distances = F.pairwise_distance(anchors_embeddings, negatives_embeddings, p=2.0)
 
-            test_loss += loss.item() / len(test_dataloader)
+            distances.extend(positive_distances.tolist())
+            distances.extend(negative_distances.tolist())
+
+            labels.extend([1] * positive_distances.size(0))  # 正例对标签为 1
+            labels.extend([0] * negative_distances.size(0))  # 负例对标签为 0
 
             total_test_step += 1
 
-    print(f"Epoch: {epoch}/{epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}")
+    # 计算阈值
+    threshold = np.percentile(distances, 50)  # 例如，使用距离的中位数作为阈值
+    # 为距离分配类别
+    predictions = [1 if d <= threshold else 0 for d in distances]
+    # 计算精度、召回率和 F1 分数
+    precision, recall, f1_score, _ = precision_recall_fscore_support(labels, predictions, average='binary')
+
+    print("学习率：{}".format(scheduler.get_lr()))
+    print("Precision: {:.4f}, Recall: {:.4f}, F1 Score: {:.4f}".format(precision, recall, f1_score))
+    print(f"Epoch: {epoch}/{epochs}, Train Loss: {train_loss:.4f}")
+
+    if train_loss < least_loss:
+        print("找到更优的训练结果,保存模型")
+        torch.save(facenet.state_dict(), "./weight/facenet_checkpoint{}.pth".format(epoch))
+        least_loss = train_loss
